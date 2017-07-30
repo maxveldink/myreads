ca1a0fe62ecfe8c1bac3c1eb692a8ad8d0fd26dd
Max VelDink <maxveldink@gmail.com>
2017-07-30 11:48:27 -0500

    refactor: Add semicolons to starter files

--- src/BooksAPI.js
+++ src/BooksAPI.js
@@ -1,28 +1,28 @@
-
-const api = "https://reactnd-books-api.udacity.com"
-
+const api = "https://reactnd-books-api.udacity.com";
 
 // Generate a unique token for storing your bookshelf data on the backend server.
-let token = localStorage.token
+let token = localStorage.token;
 if (!token)
-  token = localStorage.token = Math.random().toString(36).substr(-8)
+  token = localStorage.token = Math.random().toString(36).substr(-8);
 
 const headers = {
   'Accept': 'application/json',
   'Authorization': token
-}
+};
 
-export const get = (bookId) =>
+export const get = (bookId) => {
   fetch(`${api}/books/${bookId}`, { headers })
     .then(res => res.json())
-    .then(data => data.book)
+    .then(data => data.book);
+};
 
-export const getAll = () =>
+export const getAll = () => {
   fetch(`${api}/books`, { headers })
     .then(res => res.json())
-    .then(data => data.books)
+    .then(data => data.books);
+};
 
-export const update = (book, shelf) =>
+export const update = (book, shelf) => {
   fetch(`${api}/books/${book.id}`, {
     method: 'PUT',
     headers: {
@@ -30,9 +30,10 @@ export const update = (book, shelf) =>
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ shelf })
-  }).then(res => res.json())
+  }).then(res => res.json());
+};
 
-export const search = (query, maxResults) =>
+export const search = (query, maxResults) => {
   fetch(`${api}/search`, {
     method: 'POST',
     headers: {
@@ -40,5 +41,7 @@ export const search = (query, maxResults) =>
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ query, maxResults })
-  }).then(res => res.json())
-    .then(data => data.books)
+  })
+  .then(res => res.json())
+  .then(data => data.books);
+};
